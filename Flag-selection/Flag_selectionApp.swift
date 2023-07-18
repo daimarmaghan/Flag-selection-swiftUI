e8783d044649c8e87a3ec1b907a2e42ebe52981d
Daim <daimakram@gmail.com>
2023-07-18 00:53:13 -0700

    Initial Commit

---
+++ Flag-selection/Flag_selectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_selectionApp.swift
+//  Flag-selection
+//
+//  Created by Daim Armaghan on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Flag_selectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
